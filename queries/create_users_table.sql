687928c6c5f70f4b5a492bb24b9c0f57e0d78d02
ReplyDev <replydev@protonmail.com>
2020-11-19 17:17:39 +0100

    Fix argon2 password verify

--- queries/create_users_table.sql
+++ queries/create_users_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `users` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`password` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`password` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
